fa9c245a98e3fc6e231ca24eed80fca258f127e8
esri04 <sristark42@gmail.com>
2025-01-04 13:20:51 +0530

    explored get, post, delete

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -2,13 +2,24 @@ const express = require('express')
 const app = express();
 
 
+
+app.get('/test',(req,res)=>{
+    res.send('receieved the request')
+})
+
+app.post('/test', (req, res)=>{
+    res.send({Fname:"santhosh",Lname:'kumar'});
+})
+
+app.delete('/test',(req,res)=>{
+    res.send('data deleted successfully')
+})
+
 // '/test' is a route 
 // (req,res)=>{ res.send('hello my dear friennds')})  this is called response handler;
 app.use('/test', (req,res)=>{ 
     res.send('hello my dear friennds')
 });
-app.use('/sridar',(req,res)=>{
-    res.send('hello sridhar punda')
-})
+
 
 app.listen(3000,()=>{console.log("server is running on port 3000")});
